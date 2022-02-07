b39340b25a38ade63adaf2ae46db97ada2b69eb8
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-07 16:55:10 -0300

    Add files via upload

---
+++ Ex4_Lista1/Ex4_Lista1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex4_Lista1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a temperatura em celcius: ");
+            string strCelsius = Console.ReadLine();
+       
+            double celsius = System.Convert.ToDouble(strCelsius);
+
+            double tempEmFahreinheit = celsius * 1.8 + 32;
+
+            Console.WriteLine(celsius + "C Equivale a: " + tempEmFahreinheit + "F");
+
+        }
+    }
+}
