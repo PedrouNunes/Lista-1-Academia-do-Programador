500f1d29c4f053c06211f043b6f9fa6d52d244e1
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-07 16:59:24 -0300

    Add files via upload

---
+++ Exercício1_Lista1/Exercício1_Lista1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercício1_Lista1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o comprimento do lado 1 do retângulo: ");
+            string straltura = Console.ReadLine();
+
+            Console.WriteLine("Digite o comprimento do lado 2 do retângulo: ");
+            string strbase = Console.ReadLine();
+
+            double lado1 = System.Convert.ToDouble(straltura);
+
+            double base1 = System.Convert.ToDouble(strbase);
+
+            double conta = lado1 * base1;
+
+            Console.WriteLine("A área do terreno é de: " + conta + "m²");
+         
+        }
+    }
+}
---
+++ Exercício1_Lista1/README.md
@@ -0,0 +1,4 @@
+# Exerc-cio1_Lista1
+
+A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um
+algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno.
