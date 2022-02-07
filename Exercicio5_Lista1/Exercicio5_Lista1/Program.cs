6454fa330d08ab2ca10d304dd2e0cfe690615ed1
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-07 17:00:47 -0300

    Add files via upload

---
+++ Exercicio5_Lista1/Exercicio5_Lista1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio5_Lista1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal aumentoSalario = 0.15m;
+            const decimal impostoSalario = 0.8m;
+
+            decimal salario, aumento, desconto;
+
+            Console.WriteLine("Digite o salário do funcionário: ");
+            salario = System.Convert.ToDecimal(Console.ReadLine();
+
+            aumento = salario + (salario * aumentoSalario);
+
+            desconto = aumento - (aumento * impostoSalario);
+
+            Console.WriteLine("Salário inicial: R$" + salario + ", salário com aumento: R$" + aumento + ", salário com desconto: R$" + desconto);
+
+        }
+    }
+}
