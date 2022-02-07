7d96e0c663bd6ca7b62a93678c808910e1835ed6
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-07 16:58:18 -0300

    Add files via upload

---
+++ Exercicio3_Lista1/Exercicio3_Lista1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio3_Lista1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite quantos anos você tem: ");
+            string strAnos = Console.ReadLine();
+
+            double anos = System.Convert.ToDouble(strAnos);          
+
+            double dias = anos * 365;
+
+            Console.WriteLine(nome + ", você ja viveu " + dias + " dias");
+
+        }
+    }
+}
